0d3315f006e00fc3a30766d15566ed47c549ab4f
Maros Petro <maros.petro@firma.seznam.cz>
2025-12-30 08:05:05 +0100

    fixed bug in params

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "SNSensitivityEstimate.jl" begin
     # Write your tests here.
+    SNparams_test = SNSensitivityEstimate.SNparams
+    @test SNparams_test["Nₐ"] == 6.02214e23
 end
