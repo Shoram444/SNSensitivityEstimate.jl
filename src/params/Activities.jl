8a7b8efd4a1369d7d1ae6b8491d0dcbbef73cfee
Maros Petro <maros.petro@firma.seznam.cz>
2026-01-02 11:56:16 +0100

    updated gamma flux

--- src/params/Activities.jl
+++ src/params/Activities.jl
@@ -25,9 +25,9 @@ BkgActivityParams = Dict( #activities from Table 1 from 10.1140/epjc/s10052-018-
     :K40_hall_surface => 1.2 ,                  # [Bq] Value is taken from Xalbat (flux) divided twice by 500 - for tof cut and for shielding effect 
     :gamma_experimental_surface => 1 / 430,         # mock gammas for "somewhat flat" spectrum in nu0 ROI
     :gamma_hall_bulk => 1 / 430,         # mock gammas for "somewhat flat" spectrum in nu0 ROI
-    :Tl208_hall_bulk => 2.4,         # Xalbat's 2.4 Bq --> neutrino poster
-    :K40_hall_bulk => 1.2,         # Xalbat's 1.2 Bq --> neutrino poster
-    :Bi214_hall_bulk => 0.5,         # Xalbat's 0.5 Bq --> neutrino poster
+    :Tl208_hall_bulk => 2.4 / 500,         # Xalbat's 2.4 Bq --> neutrino poster, additionally divided by 500 for shielding factor
+    :K40_hall_bulk => 1.2 / 500,         # Xalbat's 1.2 Bq --> neutrino poster, additionally divided by 500 for shielding factor
+    :Bi214_hall_bulk => 0.5 / 500,         # Xalbat's 0.5 Bq --> neutrino poster, additionally divided by 500 for shielding factor
     :K40_calo_8inch_scin_bulk => 2.2/1000,         # [Bq/kg] from calo Federic measurements internal, docdb 3174-v2
     :Bi214_calo_8inch_scin_bulk => 0.3/1000,         # [Bq/kg] from calo Federic measurements internal, docdb 3174-v2
     )
